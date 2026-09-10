0b66f2aa67bc2dee838392f4405a639dee1479c6
Danila Lutsai <dlutsai98@gmail.com>
2026-09-10 23:40:16 +0300

    revamp

--- src/types/api.ts
+++
@@ -1,1 +0,0 @@
-// # GitHub response shapes + schemas
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-// # barrel, re-exports everything
-
-import { PROFILES, type Question, type Profile } from '@/types/quiz';
-import { TOOLS, type SetupItem, type Tool } from '@/types/setup';
-import { TECH_TAGS, type TechTag, type ContentBlock, type Project } from '@/types/content';
-import { type Async, type ProjectId, type SnippetId, projectId, snippetId, assertNever } from '@/types/common';
